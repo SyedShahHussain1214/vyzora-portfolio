333ee724fd4e0afd5bddd5ccf4c5c240cc561fae
Syed Shah Hussain Zaidi <125561928+SyedShahHussain1214@users.noreply.github.com>
2026-09-10 23:59:07 +0000

    Add Bito Screen 3 and correct AI Avatar categorization

--- api/youtube.js
+++ api/youtube.js
@@ -1,7 +1,8 @@
 const fallbackVideos = [
-  { videoId: '-JjrTeP06oA', title: 'Branding & Solutions', category: 'UGC', image: 'https://i.ytimg.com/vi/-JjrTeP06oA/hqdefault.jpg' },
+  { videoId: '-JjrTeP06oA', title: 'Branding & Solutions', category: 'AI Avatar', image: 'https://i.ytimg.com/vi/-JjrTeP06oA/hqdefault.jpg' },
   { videoId: '28gF2wVUeBM', title: 'Bito Screen 1', category: 'UGC', image: 'https://i.ytimg.com/vi/28gF2wVUeBM/hqdefault.jpg' },
   { videoId: 'YIVEPAa7FWQ', title: 'Bito Screen 2', category: 'UGC', image: 'https://i.ytimg.com/vi/YIVEPAa7FWQ/hqdefault.jpg' },
+  { videoId: 'lE6Cijf7XFM', title: 'Bito Screen 3', category: 'UGC', image: 'https://i.ytimg.com/vi/lE6Cijf7XFM/hqdefault.jpg' },
   { videoId: 'nHarODajKSU', title: 'Vyzora Super Car Showcase', category: 'CGI', image: 'https://i.ytimg.com/vi/nHarODajKSU/hqdefault.jpg' },
   { videoId: 'MiNuuXdDjWg', title: 'Studio Production Reel', category: 'CGI', image: 'https://i.ytimg.com/vi/MiNuuXdDjWg/hqdefault.jpg' },
   { videoId: 'urAsTqWD0Qg', title: 'Sakoon Perfume Campaign II', category: 'CGI', image: 'https://i.ytimg.com/vi/urAsTqWD0Qg/hqdefault.jpg' },
@@ -9,7 +10,7 @@ const fallbackVideos = [
   { videoId: 'AMY_-uERE3E', title: 'Beauty Brands Edition', category: 'CGI', image: 'https://i.ytimg.com/vi/AMY_-uERE3E/hqdefault.jpg' },
   { videoId: 'Mwct32P7F-8', title: 'Emperor Energy Drink Campaign', category: 'CGI', image: 'https://i.ytimg.com/vi/Mwct32P7F-8/hqdefault.jpg' },
   { videoId: 'kDh1X1IvjNY', title: 'Emperor Perfume Campaign', category: 'CGI', image: 'https://i.ytimg.com/vi/kDh1X1IvjNY/hqdefault.jpg' },
-  { videoId: 'gM9uw0oeHdE', title: 'Studio Production Reel II', category: 'UGC', image: 'https://i.ytimg.com/vi/gM9uw0oeHdE/hqdefault.jpg' },
+  { videoId: 'gM9uw0oeHdE', title: 'Aurelume Petal Drop Tint II', category: 'AI Avatar', image: 'https://i.ytimg.com/vi/gM9uw0oeHdE/hqdefault.jpg' },
   { videoId: 'Q_WILFZENhk', title: 'Aurelume Petal Drop Tint', category: 'AI Avatar', image: 'https://i.ytimg.com/vi/Q_WILFZENhk/hqdefault.jpg' },
   { videoId: 'ZMX_ifL0C9Y', title: 'Aurelume Luma Renew Mask', category: 'AI Avatar', image: 'https://i.ytimg.com/vi/ZMX_ifL0C9Y/hqdefault.jpg' }
 ];
